8ca012eeb32a37f9843d948a4356c5fbce8a7a5f
pauloricardocsouza <pauluricardu@gmail.com>
2026-04-28 20:08:54 -0300

    2008

--- shared.js
+++ shared.js
@@ -21,7 +21,7 @@
      - major: mudanças estruturais profundas
      - minor: correções e melhorias pontuais
      ======================================= */
-  const APP_VERSION = 'v6.5';
+  const APP_VERSION = 'v6.9';
 
   /* ========== Firebase config ==========
      SUBSTITUIR pelos valores do seu projeto
